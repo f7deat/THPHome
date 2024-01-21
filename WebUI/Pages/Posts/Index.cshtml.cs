90a9467d725d34c87580a8bca47e30049fbca0e1
f7deat <f7deat@gmail.com>
2024-01-21 17:00:24 +0700

    fix category detail breadcrumb

--- WebUI/Pages/Posts/Index.cshtml.cs
+++ WebUI/Pages/Posts/Index.cshtml.cs
@@ -2,44 +2,30 @@ using ApplicationCore.Entities;
 using ApplicationCore.Enums;
 using ApplicationCore.Interfaces.IService;
 using ApplicationCore.Models.Posts;
-using ApplicationCore.Services;
 using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WebUI.Foundations;
 
 namespace WebUI.Pages.Posts
 {
-    public class IndexModel : DynamicPageModel
+    public class IndexModel(ApplicationDbContext context, ICategoryService categoryService, IPostService postService, IAttachmentService attachmentService, ICommentService commentService) : DynamicPageModel(context)
     {
-        private readonly ICategoryService _categoryService;
-        private readonly IPostService _postService;
-        private readonly IAttachmentService _attachmentService;
-        private readonly ICommentService _commentService;
-
-        public IndexModel(ApplicationDbContext context, ICategoryService categoryService, IPostService postService, IAttachmentService attachmentService, ICommentService commentService) : base(context)
-        {
-            _categoryService = categoryService;
-            _postService = postService;
-            _attachmentService = attachmentService;
-            _commentService = commentService;
-        }
-
-        public IEnumerable<PostView> RandomPosts;
-        public IEnumerable<Post> RelatedPosts;
-        public List<Category> Categories = new List<Category>();
-        public List<Attachment> Attachments = new List<Attachment>();
+        private readonly ICategoryService _categoryService = categoryService;
+        private readonly IPostService _postService = postService;
+        private readonly IAttachmentService _attachmentService = attachmentService;
+        private readonly ICommentService _commentService = commentService;
+        public IEnumerable<PostView> RandomPosts = new List<PostView>();
+        public IEnumerable<Post> RelatedPosts = new List<Post>();
+        public List<Category> Categories = [];
+        public List<Attachment> Attachments = [];
         public IEnumerable<CommentInPost> ListComment = new List<CommentInPost>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            var Categories = await _categoryService.GetListInPostAsync(PageData.Id);
+            Categories = await _categoryService.GetListInPostAsync(PageData.Id);
             RandomPosts = await _postService.GetListByTypeAsync(PostType.NOTIFICATION, 1, 5, Language.VI);
-            if (Categories.Count() > 0)
+            if (Categories.Count > 0)
             {
                 var categoryId = Categories.Select(c => c.Id).First();
                 var relateds = from c in _context.PostCategories
