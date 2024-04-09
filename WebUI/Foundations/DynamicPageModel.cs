1dc4e07ca38648b946a954f8256e0cb5dbaffbc0
f7deat <f7deat@gmail.com>
2024-04-09 22:22:33 +0700

    search multi language

--- WebUI/Foundations/DynamicPageModel.cs
+++ WebUI/Foundations/DynamicPageModel.cs
@@ -3,41 +3,45 @@ using ApplicationCore.Interfaces.IService;
 using Infrastructure;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Threading.Tasks;
 
-namespace WebUI.Foundations
+namespace WebUI.Foundations;
+
+public abstract class DynamicPageModel : PageModel
 {
-    public abstract class DynamicPageModel : PageModel
+    protected readonly ApplicationDbContext _context;
+    public DynamicPageModel(ApplicationDbContext context)
     {
-        protected readonly ApplicationDbContext _context;
-        public DynamicPageModel(ApplicationDbContext context)
+        _context = context;
+    }
+
+    public Post PageData { protected set; get; } = new Post();
+
+    public override async Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+    {
+        context.RouteData.Values.TryGetValue("postId", out var postId);
+        if (postId is null)
         {
-            _context = context;
+            context.HttpContext.Response.StatusCode = 404;
+            context.HttpContext.Response.Redirect("/exception/notfound");
+            return;
+        }
+        _ = long.TryParse(postId.ToString(), out long id);
+        var catalog = await _context.Posts.FindAsync(id);
+        if (catalog is null)
+        {
+            context.HttpContext.Response.StatusCode = 404;
+            context.HttpContext.Response.Redirect("/exception/notfound");
+            return;
         }
 
-        public Post PageData { protected set; get; } = new Post();
+        catalog.View++;
+        _context.Update(catalog);
+        await _context.SaveChangesAsync();
 
-        public override async Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
-        {
-            context.RouteData.Values.TryGetValue("postId", out var postId);
-            long.TryParse(postId.ToString(), out long id);
-            var catalog = await _context.Posts.FindAsync(id);
-            if (catalog is null)
-            {
-                context.HttpContext.Response.StatusCode = 404;
-                context.HttpContext.Response.Redirect("/exception/notfound");
-                return;
-            }
-
-            catalog.View++;
-            _context.Update(catalog);
-            await _context.SaveChangesAsync();
-
-            PageData = catalog;
-            ViewData["Title"] = catalog.Title;
-            ViewData["Description"] = catalog.Description;
-            ViewData["Image"] = catalog.Thumbnail;
-            RouteData.Values.TryAdd(nameof(Post), PageData);
-        }
+        PageData = catalog;
+        ViewData["Title"] = catalog.Title;
+        ViewData["Description"] = catalog.Description;
+        ViewData["Image"] = catalog.Thumbnail;
+        RouteData.Values.TryAdd(nameof(Post), PageData);
     }
 }
---
+++ WebUI/Pages/Search/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using ApplicationCore.Helpers;
+using ApplicationCore.Interfaces.IService;
+using ApplicationCore.Models.Posts;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using WebUI.Foundations;
+using WebUI.Helpers;
+
+namespace WebUI.Pages.Search;
+
+public class IndexModel : EntryPageModel
+{
+
+    public IEnumerable<PostView> RandomPosts = [];
+    private readonly ICategoryService _categoryService;
+    public PaginatedList<PostView>? Posts;
+
+    public IndexModel(IPostService postService, ApplicationDbContext context, ICategoryService categoryService) : base(postService, context)
+    {
+        _categoryService = categoryService;
+    }
+
+    [BindProperty(SupportsGet = true)]
+    public string SearchTerm { get; set; } = string.Empty;
+    public int PageIndex { get; set; } = 1;
+    public int? CategoryId { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        RandomPosts = await _postService.GetRandomPostsAsync();
+
+        Request.Cookies.TryGetValue("locale", out string? locale);
+        var lang = LanguageHelper.GetLanguage(locale);
+
+        if (string.IsNullOrEmpty(SearchTerm))
+        {
+            return Page();
+        }
+        Posts = await _postService.SearchAsync(SearchTerm.ToLower(), CategoryId, PageIndex, 10);
+        return Page();
+    }
+}
