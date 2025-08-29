a4af4284e9eb72450ae39a8126bb049901d20ba3
Đỗ Đức Dũng <ducdungnaruto@gmail.com>
2025-08-29 11:29:07 +0700

    Add evidence upload functionality for foreign language proficiency; implement UI for evidence management and preview

---
+++ THPHome/Repositories/Users/ForeignLanguageRepository.cs
@@ -0,0 +1,11 @@
+﻿using THPHome.Data;
+using THPHome.Entities.Users;
+using THPHome.Interfaces.IRepository;
+using THPHome.Interfaces.IRepository.IUsers;
+using THPHome.Repositories.Base;
+
+namespace THPHome.Repositories.Users;
+
+public class ForeignLanguageRepository(ApplicationDbContext context) : EfRepository<ForeignLanguageProficiency>(context), IForeignLanguageRepository
+{
+}
