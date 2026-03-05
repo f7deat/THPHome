224b64605adf0f1b94c925de6d1cc536213128a2
f7deat <tan.dct9x@gmail.com>
2026-03-05 09:54:03 +0700

    privacy policy

---
+++ THPHome/Pages/PrivacyPolicy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace THPHome.Pages
+{
+    public class PrivacyPolicyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
